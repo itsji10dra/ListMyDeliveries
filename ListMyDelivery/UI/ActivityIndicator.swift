a0e21d60395e982cc61b43ebe7d64a106a9fba78
Jitendra <jitendra@cellpointmobile.com>
2018-09-26 10:46:43 +0530

    Fixed constraint on Activity Indicator....

--- ListMyDelivery/UI/ActivityIndicator.swift
+++ ListMyDelivery/UI/ActivityIndicator.swift
@@ -37,19 +37,29 @@ class ActivityIndicator {
             
             let view = UIView()
             view.backgroundColor = UIColor.clear
-            view.frame = UIScreen.main.bounds
             view.restorationIdentifier = restorationIdentifier
             
-            defer { keyWindow.addSubview(view) }
+            defer {
+                keyWindow.addSubview(view)
+                if let rootView = keyWindow.rootViewController?.view {
+                    view.translatesAutoresizingMaskIntoConstraints = false
+                    view.topAnchor.constraint(equalTo: rootView.topAnchor).isActive = true
+                    view.bottomAnchor.constraint(equalTo: rootView.bottomAnchor).isActive = true
+                    view.leadingAnchor.constraint(equalTo: rootView.leadingAnchor).isActive = true
+                    view.trailingAnchor.constraint(equalTo: rootView.trailingAnchor).isActive = true
+                } else {
+                    view.frame = UIScreen.main.bounds
+                }
+            }
             
             let dimView = UIView()
             dimView.backgroundColor = UIColor.black.withAlphaComponent(0.6)
             view.addSubview(dimView)
             dimView.translatesAutoresizingMaskIntoConstraints = false
-            dimView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
-            dimView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
-            dimView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
-            dimView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+            dimView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+            dimView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+            dimView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            dimView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
             
             let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
             blurView.layer.masksToBounds = true
@@ -58,8 +68,8 @@ class ActivityIndicator {
             blurView.translatesAutoresizingMaskIntoConstraints = false
             blurView.heightAnchor.constraint(equalToConstant: 150).isActive = true
             blurView.widthAnchor.constraint(equalToConstant: 150).isActive = true
-            blurView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
-            blurView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 0).isActive = true
+            blurView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            blurView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
 
             let holderStackView = UIStackView()
             holderStackView.axis = .vertical
@@ -68,8 +78,8 @@ class ActivityIndicator {
             holderStackView.spacing = 15
             blurView.contentView.addSubview(holderStackView)
             holderStackView.translatesAutoresizingMaskIntoConstraints = false
-            holderStackView.centerXAnchor.constraint(equalTo: blurView.centerXAnchor, constant: 0).isActive = true
-            holderStackView.centerYAnchor.constraint(equalTo: blurView.centerYAnchor, constant: 0).isActive = true
+            holderStackView.centerXAnchor.constraint(equalTo: blurView.centerXAnchor).isActive = true
+            holderStackView.centerYAnchor.constraint(equalTo: blurView.centerYAnchor).isActive = true
             
             let loaderView = UIActivityIndicatorView(style: .whiteLarge)
             loaderView.startAnimating()
