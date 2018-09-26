0118d30871f4a0fbfb46067cdd637c6d48199d65
Jitendra <jitendra@cellpointmobile.com>
2018-09-27 00:51:42 +0530

    Added check to avoid loading of app delegate on test run...

--- ListMyDelivery/AppDelegate.swift
+++ ListMyDelivery/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+        guard UserDefaults.standard.bool(forKey: "isRunningTest") == false else { return true }
+        
         Configuration.checkConfiguration()
         _ = ReachabilityManager.shared
         
